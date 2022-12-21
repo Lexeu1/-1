fb7d5572a3582c4f3b419120ba51a4b883753338
Lexeu1 <119842347+Lexeu1@users.noreply.github.com>
2022-12-21 16:01:29 +0600

    Add files via upload

---
+++ экзамен_слож/экзамен_слож/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace экзамен_слож
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            Window1 taskWindow = new Window1();
+            taskWindow.Show();
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //    using System;
+        //using System.Data.Entity;
+        //using System.Data.Entity.ModelConfiguration;
+        //public partial class ToursBaseEntities : DbContext
+        //{
+        //    private static ToursBaseEntities _context;
+        //    public ToursBaseEntities()
+        //       : base("name = ToursBaseEntities")
+        //    {
+
+        //    }
+        //    public static ToursBaseEntities GetContext()
+        //    {
+        //        if (_context == null)
+        //            _context = new ToursBaseEntities();
+        //        return _context;
+        //}
+    }
+}
